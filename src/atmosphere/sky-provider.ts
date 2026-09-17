1aa0bc3cbed273dbf751e9af79e446f6de105c90
Adam NAILI <adam.naili.pro@gmail.com>
2026-09-17 16:45:13 +0200

    fix: support host NodeBuilder declarations

--- src/atmosphere/sky-provider.ts
+++ src/atmosphere/sky-provider.ts
@@ -154,7 +154,7 @@ class AnalyticEnvironmentNode extends TempNode<'vec3'> {
   }
 
   override setup(builder: NodeBuilder): Node {
-    const context = builder.context as EnvironmentContext
+    const context = (builder as NodeBuilder & { context: EnvironmentContext }).context
     const direction = context.getUV?.(this, builder)
     if (!direction) return TSL.vec3(0)
 
